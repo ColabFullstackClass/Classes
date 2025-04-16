cb1ad9c868f8943a657902a8609baa285a1f6113
Isaac <edokaizkdaking247@gmail.com>
2025-04-16 14:27:18 +0100

    fix:fix renderscreen bug in dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,10 @@
 let body=document.getElementById('body')
-let p=document.createElement('h1');
-let desc=document.createElement('p')
-let div=document.createElement('div');
+
 export function updateScreenwithTasks(tasks){
     tasks.map(task=>{
+        let p=document.createElement('h1');
+        let desc=document.createElement('p')
+        let div=document.createElement('div');
         p.textContent=`${task.name}\n`;
         desc.textContent=`${task.description}`
         div.append(p)
